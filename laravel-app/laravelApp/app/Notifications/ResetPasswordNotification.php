e346a0ba9bb5d3ea51d8c96c1329687db7fe494f
kongchanlina <kongchanlina3@gmail.com>
2026-07-20 09:04:56 +0700

    session3.6 ShouldQueue

--- laravel-app/laravelApp/app/Notifications/ResetPasswordNotification.php
+++ laravel-app/laravelApp/app/Notifications/ResetPasswordNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Mail;
 
-class ResetPasswordNotification extends Notification
+class ResetPasswordNotification extends Notification implements ShouldQueue
 {
     use Queueable;
     private $token;
