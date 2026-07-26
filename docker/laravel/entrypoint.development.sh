0b63a01f8e682033a83fd702d63329f461bbb084
kongchanlina <kongchanlina3@gmail.com>
2026-07-27 02:04:34 +0700

    session3.7 + roadrunner: preparing for hosting

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -7,6 +7,4 @@ php artisan key:generate
 wait $!
 php artisan migrate
 wait $!
-php artisan serve --host=0.0.0.0 --port=8000 &
-
-php artisan queue:work --tries=3
+exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
